eb2d4b571c6b3299e6911d15be549e8ee245eff6
dhl7799 <donghyundhl@naver.com>
2023-01-19 15:02:15 +0900

    jstl

--- guestbook02/src/main/webapp/WEB-INF/views/index.jsp
+++ guestbook02/src/main/webapp/WEB-INF/views/index.jsp
@@ -8,7 +8,6 @@
 
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%
-	//"
 	request.setCharacterEncoding("utf-8");
 	List<guestbookVo> list = new guestbookDao().showAll();
 %>
---
+++ jstlel/src/main/java/jstlel/_01Servlet.java
@@ -0,0 +1,38 @@
+package jstlel;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class _01Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int iVal = 10;
+		long lVal = 10L;
+		float fVal = 3.14f;
+		boolean bVal = true;
+		String sVal = "가나다라마바사";
+		
+		request.setAttribute("iVal", iVal);
+		request.setAttribute("lVal", lVal);
+		request.setAttribute("fVal", fVal);
+		request.setAttribute("bVal", bVal);
+		request.setAttribute("sVal", sVal);
+		
+		Object obj = null;
+		
+		request.setAttribute("obj", obj);
+		
+		request.getRequestDispatcher("/WEB-INF/views/01.jsp")
+		.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
